e51d8ec5b46d723a470580e17b2b0054b051b65d
DFcruzz <diegofcruzz@gmail.com>
2022-06-02 16:51:23 -0300

    round scores values added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-let playerChoice = prompt("Rock, Paper or Scissors?")
 let playerScore = 0
 let computerScore = 0
+let playerChoice = prompt("What's you choice: Rock, Paper or Scissors?")
 
+// Function to make the computer returns a random value
 function computerChoice() {
     let options = ['rock', 'paper', 'scissors']
     return options[Math.floor(Math.random() * options.length)]
 }
 
+// Round function that compares the player choice (prompt on page load) to computer random choice
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerChoice();
     playerSelection = playerChoice;
-
 
     if (playerSelection == computerSelection) {
         console.log('This Round was a tie!');
@@ -19,12 +20,30 @@ function playRound(playerSelection, computerSelection) {
     else if ((playerSelection == 'rock' && computerSelection == 'scissors')||
     (playerSelection == 'scissors' && computerSelection == 'paper')||
     (playerSelection == 'paper' && computerSelection == 'rock')) {
-        console.log(playerSelection + ' beats ' + computerSelection + '\nYou have WON this round!');
+
+        playerScore++;
+
+        console.log(playerSelection + " beats " + computerSelection + "\nYou have WON this round!");
+
+        if (playerScore == 5) {
+            console.log("Congratulations you've WON the GAME!!!")
+        }
     }
 
     else {
-        console.log(playerSelection + ' loses to ' + computerSelection + '\nYou have LOST this round!')
+
+        computerScore++;
+
+        console.log(playerSelection + " loses to " + computerSelection + "\nYou have LOST this round!")
+        
+        if (computerScore == 5) {
+            console.log("Ow Shoot, you've LOST the GAME!")
+        }
+    
     }
 }
 
-console.log(playRound())
+// Prints round result and current score
+console.log(playRound());
+console.log("Computer Score: " + computerScore);
+console.log("Player Score: " + playerScore);
